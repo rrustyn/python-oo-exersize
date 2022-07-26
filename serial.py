d0416503ed6b841e4957cf0b01fa6c85ebb39f33
Ryan Rustyn <ryanrustyn@gmail.com>
2022-07-26 15:33:27 -0700

    wrote functionality

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    # make two methods __init__ generate and reset
+    def __init__(self, start):
+        """Saving the start value and making an incrementer"""
+        self.start = start
+        self.current = start - 1
+
+    def generate(self):
+        """Increments current by one"""
+        self.current += 1
+        return self.current
+
+    def reset(self):
+        """Resets incrementer"""
+        self.current = self.start - 1
