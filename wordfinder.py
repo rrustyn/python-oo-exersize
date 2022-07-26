3f8fbc12253e054defcff2e736c0b241af90ca85
Ryan Rustyn <ryanrustyn@gmail.com>
2022-07-26 16:06:46 -0700

    wordfiner init and read_file

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,11 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.word_list = []
+        self.read_file()
+
+    def read_file(self):
+        with open(self.filepath, "r") as file:
+            for line in file:
+                self.word_list.append(line)
